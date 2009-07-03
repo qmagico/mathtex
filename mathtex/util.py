d38f73ce51ad998447c4d222b2660684651b6a6b
freddie <freddie@witherden.org>
2009-07-03 14:41:18 +0000

    Add a modified version of matplotlibs font manager to mathtex. Although currently unused the AFM code is still intact for later use. The caching mechanism is the same as matplotlibs. Some rcParams reference still remain.

--- mathtex/util.py
+++ mathtex/util.py
@@ -1,3 +1,5 @@
+import os, tempfile
+
 class Bunch:
     """
     Often we want to just collect a bunch of stuff together, naming each
@@ -18,3 +20,74 @@ class Bunch:
     def __repr__(self):
         keys = self.__dict__.keys()
         return 'Bunch(%s)'%', '.join(['%s=%s'%(k,self.__dict__[k]) for k in keys])
+
+def get_configdir():
+    """
+    Return the string representing the configuration dir.
+
+    default is HOME/.matplotlib.  you can override this with the
+    MATHTEXCONFIGDIR environment variable
+    """
+
+    configdir = os.environ.get('MATHTEXCONFIGDIR')
+    if configdir is not None:
+        if not _is_writable_dir(configdir):
+            raise RuntimeError('Could not write to MATHTEXCONFIGDIR="%s"'%configdir)
+        return configdir
+
+    h = get_home()
+    p = os.path.join(get_home(), '.mathtex')
+
+    if os.path.exists(p):
+        if not _is_writable_dir(p):
+            raise RuntimeError("'%s' is not a writable dir; you must set %s/.mathtex to be a writable dir.  You can also set environment variable MATHTEXCONFIGDIR to any writable directory where you want mathtex data stored "% (h, h))
+    else:
+        if not _is_writable_dir(h):
+            raise RuntimeError("Failed to create %s/.mathtex; consider setting MATHTEXCONFIGDIR to a writable directory for matplotlib configuration data"%h)
+
+        os.mkdir(p)
+
+    return p
+
+def get_home():
+    """
+    Find user's home directory if possible.
+    Otherwise raise error.
+
+    :see:  http://mail.python.org/pipermail/python-list/2005-February/263921.html
+    """
+    path=''
+    try:
+        path=os.path.expanduser("~")
+    except:
+        pass
+    if not os.path.isdir(path):
+        for evar in ('HOME', 'USERPROFILE', 'TMP'):
+            try:
+                path = os.environ[evar]
+                if os.path.isdir(path):
+                    break
+            except: pass
+    if path:
+        return path
+    else:
+        raise RuntimeError('please define environment variable $HOME')
+
+def is_string_like(obj):
+    try: obj + ''
+    except (TypeError, ValueError): return 0
+    return 1
+
+def _is_writable_dir(p):
+    """
+    p is a string pointing to a putative writable dir -- return True p
+    is such a string, else False
+    """
+    try: p + ''  # test is string like
+    except TypeError: return False
+    try:
+        t = tempfile.TemporaryFile(dir=p)
+        t.write('1')
+        t.close()
+    except OSError: return False
+    else: return True
