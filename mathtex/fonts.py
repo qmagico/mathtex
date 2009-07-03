5b0791e66028f1607828b0358adbefa0bdb4a360
freddie <freddie@witherden.org>
2009-07-03 18:09:38 +0000

    Add support for unicode and STIX fonts using the code from matplotlib. Rendering is similar to that of matplotlib but square-roots are currently incorrect.

--- mathtex/fonts.py
+++ mathtex/fonts.py
@@ -1,9 +1,41 @@
 from mathtex.util import Bunch
 from mathtex.data import latex_to_bakoma, \
-        latex_to_standard, tex2uni, latex_to_cmex
+        latex_to_standard, tex2uni, latex_to_cmex, stix_virtual_fonts
 
 from mathtex.ft2font import FT2Font
-from mathtex.font_manager import findfont
+from mathtex.font_manager import findfont, FontProperties
+
+from warnings import warn
+import unicodedata
+
+def get_unicode_index(symbol):
+    """
+    get_unicode_index(symbol) -> integer
+
+    Return the integer index (from the Unicode table) of symbol.  *symbol*
+    can be a single unicode character, a TeX command (i.e. r'\pi'), or a
+    Type1 symbol name (i.e. 'phi').
+    """
+    # From UTF #25: U+2212 minus sign is the preferred
+    # representation of the unary and binary minus sign rather than
+    # the ASCII-derived U+002D hyphen-minus, because minus sign is
+    # unambiguous and because it is rendered with a more desirable
+    # length, usually longer than a hyphen.
+    if symbol == '-':
+        return 0x2212
+    try:# This will succeed if symbol is a single unicode char
+        return ord(symbol)
+    except TypeError:
+        pass
+    try:# Is symbol a TeX symbol (i.e. \alpha)
+        return tex2uni[symbol.strip("\\")]
+    except KeyError:
+        message = """'%(symbol)s' is not a valid Unicode character or
+TeX/Type1 symbol"""%locals()
+        raise ValueError, message
+
+class MathTextWarning(Warning):
+    pass
 
 class Fonts(object):
     """
@@ -264,3 +296,222 @@ class BakomaFonts(TruetypeFonts):
 
     def get_sized_alternatives_for_symbol(self, fontname, sym):
         return self._size_alternatives.get(sym, [(fontname, sym)])
+
+class UnicodeFonts(TruetypeFonts):
+    """
+    An abstract base class for handling Unicode fonts.
+
+    While some reasonably complete Unicode fonts (such as DejaVu) may
+    work in some situations, the only Unicode font I'm aware of with a
+    complete set of math symbols is STIX.
+
+    This class will "fallback" on the Bakoma fonts when a required
+    symbol can not be found in the font.
+    """
+    use_cmex = True
+
+    def __init__(self, *args, **kwargs):
+        # This must come first so the backend's ownMathTextWarninger is set correctly
+        if False: #rcParams['mathtext.fallback_to_cm']:
+            self.cm_fallback = BakomaFonts(*args, **kwargs)
+        else:
+            self.cm_fallback = None
+        TruetypeFonts.__init__(self, *args, **kwargs)
+        self.fontmap = {}
+        for texfont in "cal rm tt it bf sf".split():
+            #prop = rcParams['mathtext.' + texfont]
+            prop = 'serif'
+            font = findfont(prop)
+            self.fontmap[texfont] = font
+        prop = FontProperties('cmex10')
+        font = findfont(prop)
+        self.fontmap['ex'] = font
+
+    _slanted_symbols = set(r"\int \oint".split())
+
+    def _map_virtual_font(self, fontname, font_class, uniindex):
+        return fontname, uniindex
+
+    def _get_glyph(self, fontname, font_class, sym, fontsize):
+        found_symbol = False
+
+        if self.use_cmex:
+            uniindex = latex_to_cmex.get(sym)
+            if uniindex is not None:
+                fontname = 'ex'
+                found_symbol = True
+
+        if not found_symbol:
+            try:
+                uniindex = get_unicode_index(sym)
+                found_symbol = True
+            except ValueError:
+                uniindex = ord('?')
+                warn("No TeX to unicode mapping for '%s'" %
+                     sym.encode('ascii', 'backslashreplace'),
+                     MathTextWarning)
+
+        fontname, uniindex = self._map_virtual_font(
+            fontname, font_class, uniindex)
+
+        # Only characters in the "Letter" class should be italicized in 'it'
+        # mode.  Greek capital letters should be Roman.
+        if found_symbol:
+            new_fontname = fontname
+
+            if fontname == 'it':
+                if uniindex < 0x10000:
+                    unistring = unichr(uniindex)
+                    if (not unicodedata.category(unistring)[0] == "L"
+                        or unicodedata.name(unistring).startswith("GREEK CAPITAL")):
+                        new_fontname = 'rm'
+
+            slanted = (new_fontname == 'it') or sym in self._slanted_symbols
+            found_symbol = False
+            try:
+                cached_font = self._get_font(new_fontname)
+            except RuntimeError:
+                pass
+            else:
+                try:
+                    glyphindex = cached_font.charmap[uniindex]
+                    found_symbol = True
+                except KeyError:
+                    pass
+
+        if not found_symbol:
+            if self.cm_fallback:
+                warn("Substituting with a symbol from Computer Modern.",
+                     MathTextWarning)
+                return self.cm_fallback._get_glyph(
+                    fontname, 'it', sym, fontsize)
+            else:
+                if fontname in ('it', 'regular') and isinstance(self, StixFonts):
+                    return self._get_glyph('rm', font_class, sym, fontsize)
+                warn("Font '%s' does not have a glyph for '%s'" %
+                     (fontname, sym.encode('ascii', 'backslashreplace')),
+                     MathTextWarning)
+                warn("Substituting with a dummy symbol.", MathTextWarning)
+                fontname = 'rm'
+                new_fontname = fontname
+                cached_font = self._get_font(fontname)
+                uniindex = 0xA4 # currency character, for lack of anything better
+                glyphindex = cached_font.charmap[uniindex]
+                slanted = False
+
+        symbol_name = cached_font.font.get_glyph_name(glyphindex)
+        return cached_font, uniindex, symbol_name, fontsize, slanted
+
+    def get_sized_alternatives_for_symbol(self, fontname, sym):
+        if self.cm_fallback:
+            return self.cm_fallback.get_sized_alternatives_for_symbol(
+                fontname, sym)
+        return [(fontname, sym)]
+
+class StixFonts(UnicodeFonts):
+    """
+    A font handling class for the STIX fonts.
+
+    In addition to what UnicodeFonts provides, this class:
+
+    - supports "virtual fonts" which are complete alpha numeric
+      character sets with different font styles at special Unicode
+      code points, such as "Blackboard".
+
+    - handles sized alternative characters for the STIXSizeX fonts.
+    """
+    _fontmap = { 'rm'  : 'STIXGeneral',
+                 'it'  : 'STIXGeneral:italic',
+                 'bf'  : 'STIXGeneral:weight=bold',
+                 'nonunirm' : 'STIXNonUnicode',
+                 'nonuniit' : 'STIXNonUnicode:italic',
+                 'nonunibf' : 'STIXNonUnicode:weight=bold',
+
+                 0 : 'STIXGeneral',
+                 1 : 'STIXSize1',
+                 2 : 'STIXSize2',
+                 3 : 'STIXSize3',
+                 4 : 'STIXSize4',
+                 5 : 'STIXSize5'
+                 }
+    use_cmex = False
+    cm_fallback = False
+    _sans = False
+
+    def __init__(self, *args, **kwargs):
+        TruetypeFonts.__init__(self, *args, **kwargs)
+        self.fontmap = {}
+        for key, name in self._fontmap.iteritems():
+            fullpath = findfont(name)
+            self.fontmap[key] = fullpath
+            self.fontmap[name] = fullpath
+
+    def _map_virtual_font(self, fontname, font_class, uniindex):
+        # Handle these "fonts" that are actually embedded in
+        # other fonts.
+        mapping = stix_virtual_fonts.get(fontname)
+        if (self._sans and mapping is None and
+            fontname not in ('regular', 'default')):
+            mapping = stix_virtual_fonts['sf']
+            doing_sans_conversion = True
+        else:
+            doing_sans_conversion = False
+
+        if mapping is not None:
+            if isinstance(mapping, dict):
+                mapping = mapping.get(font_class, 'rm')
+
+            # Binary search for the source glyph
+            lo = 0
+            hi = len(mapping)
+            while lo < hi:
+                mid = (lo+hi)//2
+                range = mapping[mid]
+                if uniindex < range[0]:
+                    hi = mid
+                elif uniindex <= range[1]:
+                    break
+                else:
+                    lo = mid + 1
+
+            if uniindex >= range[0] and uniindex <= range[1]:
+                uniindex = uniindex - range[0] + range[3]
+                fontname = range[2]
+            elif not doing_sans_conversion:
+                # This will generate a dummy character
+                uniindex = 0x1
+                fontname = rcParams['mathtext.default']
+
+        # Handle private use area glyphs
+        if (fontname in ('it', 'rm', 'bf') and
+            uniindex >= 0xe000 and uniindex <= 0xf8ff):
+            fontname = 'nonuni' + fontname
+
+        return fontname, uniindex
+
+    _size_alternatives = {}
+    def get_sized_alternatives_for_symbol(self, fontname, sym):
+        alternatives = self._size_alternatives.get(sym)
+        if alternatives:
+            return alternatives
+
+        alternatives = []
+        try:
+            uniindex = get_unicode_index(sym)
+        except ValueError:
+            return [(fontname, sym)]
+
+        fix_ups = {
+            ord('<'): 0x27e8,
+            ord('>'): 0x27e9 }
+
+        uniindex = fix_ups.get(uniindex, uniindex)
+
+        for i in range(6):
+            cached_font = self._get_font(i)
+            glyphindex = cached_font.charmap.get(uniindex)
+            if glyphindex is not None:
+                alternatives.append((i, unichr(uniindex)))
+
+        self._size_alternatives[sym] = alternatives
+        return alternatives
