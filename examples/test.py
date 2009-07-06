d8e82e3f06ce3e49a9e75f428acb0a771f45de5d
freddie <freddie@witherden.org>
2009-07-06 21:59:01 +0000

    Update the example test script.

--- examples/test.py
+++ examples/test.py
@@ -4,19 +4,23 @@
 from mathtex.parser import MathtexParser
 from mathtex.boxmodel import ship
 from mathtex.fonts import BakomaFonts
+from mathtex.fonts import StixFonts
 from mathtex.backends.backend_cairo import MathtexBackendCairo
 from mathtex.backends.backend_image import MathtexBackendImage
 
 parser = MathtexParser()
 bakoma = BakomaFonts()
+stix = StixFonts()
 
 box =  parser.parse("$\sqrt{x}$", bakoma, 12, 99.0)
 
-print box
+#print box
 
-rects, glyphs =  ship(0, -box.depth, box)
+rects, glyphs, bbox =  ship(0, 0, box)
 
-print glyphs
+width = bbox[2] - bbox[0]
+height = box.height
+depth = box.depth
 
 backend = MathtexBackendImage(99.0)
 backend.set_canvas_size(box.width, box.height, box.depth)
