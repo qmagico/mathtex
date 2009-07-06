309bfcdea403fc2a010b9c85aa042dc6363076f5
freddie <freddie@witherden.org>
2009-07-06 09:53:45 +0000

    Add support for STIX and other unicode fonts to the Cairo backend.

--- mathtex/backends/backend_cairo.py
+++ mathtex/backends/backend_cairo.py
@@ -5,6 +5,8 @@ Requires: cairo, pycairo
 """
 from math import ceil
 
+from mathtex.font_manager import ttfFontProperty
+
 try:
     import cairo
 except ImportError:
@@ -16,6 +18,32 @@ class MathtexBackendCairo(MathtexBackend):
     """
     A Cairo backend for Mathtex.
     """
+    fontweights = {
+        100          : cairo.FONT_WEIGHT_NORMAL,
+        200          : cairo.FONT_WEIGHT_NORMAL,
+        300          : cairo.FONT_WEIGHT_NORMAL,
+        400          : cairo.FONT_WEIGHT_NORMAL,
+        500          : cairo.FONT_WEIGHT_NORMAL,
+        600          : cairo.FONT_WEIGHT_BOLD,
+        700          : cairo.FONT_WEIGHT_BOLD,
+        800          : cairo.FONT_WEIGHT_BOLD,
+        900          : cairo.FONT_WEIGHT_BOLD,
+        'ultralight' : cairo.FONT_WEIGHT_NORMAL,
+        'light'      : cairo.FONT_WEIGHT_NORMAL,
+        'normal'     : cairo.FONT_WEIGHT_NORMAL,
+        'medium'     : cairo.FONT_WEIGHT_NORMAL,
+        'semibold'   : cairo.FONT_WEIGHT_BOLD,
+        'bold'       : cairo.FONT_WEIGHT_BOLD,
+        'heavy'      : cairo.FONT_WEIGHT_BOLD,
+        'ultrabold'  : cairo.FONT_WEIGHT_BOLD,
+        'black'      : cairo.FONT_WEIGHT_BOLD,
+        }
+
+    fontangles = {
+        'italic'  : cairo.FONT_SLANT_ITALIC,
+        'normal'  : cairo.FONT_SLANT_NORMAL,
+        'oblique' : cairo.FONT_SLANT_OBLIQUE,
+        }
 
     def __init__(self, dpi):
         self._rendered = False
@@ -23,12 +51,12 @@ class MathtexBackendCairo(MathtexBackend):
 
     def render(self, glyphs, rects):
         # Extract the info Cairo needs to render the equation
-        self._glyphs = [(info.postscript_name, info.fontsize, unichr(info.num),
+        self._glyphs = [(info.font, info.fontsize, unichr(info.num),
                          ox, oy - info.offset)
                         for ox, oy, info in glyphs]
         self._rects = [(x1, y1, x2 - x1, y2 - y1)
                        for x1, y1, x2, y2 in rects]
-        
+
         self._rendered = True
 
     def get_formats(self):
@@ -47,14 +75,15 @@ class MathtexBackendCairo(MathtexBackend):
         """
         ctx.save()
 
-        for name, fontsize, s, ox, oy in self._glyphs:
+        for font, fontsize, s, ox, oy in self._glyphs:
             ctx.new_path()
             ctx.move_to(ox, oy)
 
+            fontProp = ttfFontProperty(font)
             ctx.save()
-            ctx.select_font_face(name.lower(),
-                                 cairo.FONT_SLANT_NORMAL,
-                                 cairo.FONT_WEIGHT_NORMAL)
+            ctx.select_font_face (fontProp.name,
+                                 self.fontangles [fontProp.style],
+                                 self.fontweights[fontProp.weight])
             size = fontsize * self.dpi / 72.0
             ctx.set_font_size(size)
             ctx.show_text(s.encode("utf-8"))
