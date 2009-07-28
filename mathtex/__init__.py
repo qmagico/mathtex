3adda04def94f2f3af5822b6be672628ebf9c005
freddie <freddie@witherden.org>
2009-07-28 15:29:48 +0000

    Update the version and change log; preparing for 0.1rc2.

--- mathtex/__init__.py
+++ mathtex/__init__.py
@@ -3,6 +3,6 @@ Mathtex is a Python library for typesetting mathematical equations written in
 LaTeX.
 """
 
-__version__ = "0.1-svn"
+__version__ = "0.1rc2"
 
 #from mathtex.mathtex_main import Mathtex
